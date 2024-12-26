910cec9f90ad271e2a57fd8fc182df1ceb9d12e9
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-12-27 02:57:40 +0530

    initialize CRM project with Gradle setup and basic structure

---
+++ src/test/java/com/abhijithanandan/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhijithanandan.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
